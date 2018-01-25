7c7ec1a1f67bcbfad90d5e92967bd1f42787678c
Zackyization <zackyization@gmail.com>
2018-01-25 23:57:35 +0800

    Initial Commit

---
+++ nxtbus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nxtbus
+//
+//  Created by Zildjian Garcia on 25/1/18.
+//  Copyright © 2018 Zildjian Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nxtbus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nxtbus
+//
+//  Created by Zildjian Garcia on 25/1/18.
+//  Copyright © 2018 Zildjian Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
